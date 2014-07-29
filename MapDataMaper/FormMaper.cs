ce5ab821bcbfca68df62f41c9555cb3fd50c4e00
fbutts <fbutts@yahoo.com>
2014-07-28 20:32:18 -0400

    Initial Load

---
+++ MapDataMaper/FormMaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//testtesttest
+
+namespace MapDataMaper
+{
+    class FormMaper
+    {
+    }
+}
---
+++ MapDataMaper/GetMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//test
+
+namespace MapDataMaper
+{
+    public class GetMap
+    {
+        public DataTable getMap()
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adapter;
+            Connection dataConnecter = new Connection();
+
+            SqlConnection con = new SqlConnection(dataConnecter.createSqlServerConnection("test","teset","1234","test"));
+            SqlCommand com = new SqlCommand();
+
+            com.CommandText = "MapProcedure";
+            com.CommandType = CommandType.StoredProcedure;
+
+            com.Connection = con;
+
+
+            adapter = new SqlDataAdapter(com);
+            adapter.Fill(dt);
+
+            con.Close();
+
+
+            return dt;
+        }
+
+
+    }
+}
